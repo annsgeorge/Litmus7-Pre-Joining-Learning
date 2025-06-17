ad6fabf208a3062fc4a33839208d2835a719a440
annsgeorge <114213540+annsgeorge@users.noreply.github.com>
2025-06-17 16:08:22 +0530

    Delete doc directory

--- doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.litmus7.studentdetail","c":"StudentApp","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"com.litmus7.studentdetail","c":"StudentApp","l":"StudentApp()","u":"%3Cinit%3E()"}];updateSearchResults();
--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.litmus7.studentdetail","l":"StudentApp"}];updateSearchResults();
